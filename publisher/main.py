70457d40dbebd063818c7d2667c6d002c39efac5
Alexander Medvedko <alexmed2000@mail.ru>
2023-01-05 22:52:41 +0300

    Added rabbitmq cluster and haproxy

--- publisher/main.py
+++ publisher/main.py
@@ -1,6 +1,8 @@
 import os.path
 import pika
 import time
+import sys
+import traceback
 
 
 def get_publishing_speed():
@@ -18,19 +20,23 @@ def get_publishing_speed():
 
 def start_publishing():
     print("Start publishing")
-    rmq_url_connection_str = 'amqp://rmuser:rmpassword@rabbitmq:5672/'
-    rmq_parameters = pika.URLParameters(rmq_url_connection_str)
-    rmq_connection = pika.BlockingConnection(rmq_parameters)
-    rmq_channel = rmq_connection.channel()
     i = 1
     while True:
-        sleep_time = get_publishing_speed()
-        rmq_channel.basic_publish(exchange='first', routing_key='testkey', body=str(i).encode("utf-8"))
-        print(f'Sending message {i}')
-        i = i + 1
-        time.sleep(sleep_time)
-    rmq_connection.close()
-
+        try:
+            rmq_url_connection_str = 'amqp://rmuser:rmpassword@rabbitmq:5672/'
+            rmq_parameters = pika.URLParameters(rmq_url_connection_str)
+            rmq_connection = pika.BlockingConnection(rmq_parameters)
+            rmq_channel = rmq_connection.channel()
+            sleep_time = get_publishing_speed()
+            rmq_channel.basic_publish(exchange='first', routing_key='testkey', body=str(i).encode("utf-8"))
+            print(f'Sending message {i}')
+            i = i + 1
+            time.sleep(sleep_time)
+            rmq_connection.close()
+        except Exception:
+            error_trace = traceback.format_exc()
+            print(f'Ошибка:{error_trace}\n')
+            sys.stderr.write(f'Ошибка:{error_trace}\n')
 
 if __name__ == '__main__':
     print("Starting ...")
